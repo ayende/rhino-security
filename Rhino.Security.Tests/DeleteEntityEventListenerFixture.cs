e8ac66754dfc7fdfb9985e25143325e377f51351
Dan Smith <sonomofo@gmail.com>
2010-09-28 10:33:29 -0400

    Event listener to delete security information when an entity is deleted from the system.

---
+++ Rhino.Security.Tests/DeleteEntityEventListenerFixture.cs
@@ -0,0 +1,39 @@
+﻿namespace Rhino.Security.Tests
+{
+	using System;
+	using NHibernate.Criterion;
+	using Rhino.Security.Model;
+	using Xunit;
+
+	public class DeleteEntityEventListenerFixture : DatabaseFixture
+	{
+		[Fact]
+		public void DoesDeletingEntityRemoveEntityReferences() {
+			var account = new Account() { Name = "Bob" };
+			session.Save(account);
+			authorizationRepository.AssociateEntityWith(account, "Important Accounts");
+			session.Flush();
+
+			Guid securityKey = Security.ExtractKey(account);
+
+			// Confirm EntityReference for the new account exists.
+			EntityReference reference = session.CreateCriteria<EntityReference>()
+				 .Add(Restrictions.Eq("EntitySecurityKey", securityKey))
+				 .SetCacheable(true)
+				 .UniqueResult<EntityReference>();
+
+			Assert.NotNull(reference);
+
+			// Delete account and confirm EntityReference was removed
+			session.Delete(account);
+			session.Flush();
+
+			reference = session.CreateCriteria<EntityReference>()
+				 .Add(Restrictions.Eq("EntitySecurityKey", securityKey))
+				 .SetCacheable(true)
+				 .UniqueResult<EntityReference>();
+
+			Assert.Null(reference);
+		}
+	}
+}
---
+++ Rhino.Security/DeleteEntityEventListener.cs
@@ -0,0 +1,47 @@
+﻿namespace Rhino.Security
+{
+	using System;
+	using NHibernate.Event;
+	using Microsoft.Practices.ServiceLocation;
+	using Rhino.Security.Interfaces;
+	using System.Collections.Generic;
+	using Rhino.Security.Model;
+	using Rhino.Security.Services;
+	using NHibernate.Criterion;
+
+	/// <summary>
+	/// Litenens for when a secured entity is deleted from the system and deletes 
+	/// associated security data.
+	/// </summary>
+	[Serializable]
+	public class DeleteEntityEventListener : IPreDeleteEventListener
+	{
+		#region IPreDeleteEventListener Members
+
+		/// <summary>
+		/// Handles PreDelete event to delete an entity's associated security data.
+		/// </summary>
+		/// <param name="deleteEvent">Event object containing the delete operation information.</param>
+		/// <returns>False, indicating the delete operation should not be vetoed.</returns>
+		public bool OnPreDelete(PreDeleteEvent deleteEvent) {
+			var securityKey = Security.ExtractKey(deleteEvent.Entity);
+
+			if (!Guid.Empty.Equals(securityKey)) {
+				EntityReference reference = deleteEvent.Session.CreateCriteria<EntityReference>()
+					 .Add(Restrictions.Eq("EntitySecurityKey", securityKey))
+					 .SetCacheable(true)
+					 .UniqueResult<EntityReference>();
+
+				if (reference != null) {
+					var childSession = deleteEvent.Session.GetSession(NHibernate.EntityMode.Poco);
+					childSession.Delete(reference);
+					childSession.Flush();
+				}
+			}
+
+			return false;
+		}
+
+		#endregion
+	}
+}
