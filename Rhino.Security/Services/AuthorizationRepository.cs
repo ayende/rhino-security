360108b9d198e9215ef7dbd08e6eb15ba37c932a
Dan Smith <sonomofo@gmail.com>
2010-09-28 10:20:48 -0400

    Undo previous fix; removed the RemoveOrphanedEntityReference method from AuthorizationRepository
    - Will use event listener to fix bug

--- Rhino.Security/Services/AuthorizationRepository.cs
+++ Rhino.Security/Services/AuthorizationRepository.cs
@@ -501,7 +501,6 @@ namespace Rhino.Security.Services
 
 			EntityReference reference = GetOrCreateEntityReference<TEntity>(key);
 			entitiesGroup.Entities.Remove(reference);
-			RemoveOrphanedEntityReference<TEntity>(entity, reference);
 		}
 
 
@@ -588,14 +587,6 @@ namespace Rhino.Security.Services
 			    session.Save(entityType);
 			}
 			return entityType;
-		}
-
-		private void RemoveOrphanedEntityReference<TEntity>(TEntity entity, EntityReference reference) where TEntity : class 
-		{
-			EntitiesGroup[] entitiesGroups = this.GetAssociatedEntitiesGroupsFor<TEntity>(entity);
-			if (entitiesGroups.Length <= 1) {
-				session.Delete(reference);
-			}
 		}
 	}
 }
