771d74a087785da5ac30c5376779aaa4216f566a
Dan Smith <sonomofo@gmail.com>
2010-10-25 14:42:34 -0400

    Added code to DeleteEntryEventListener to also delete Permissions and EntitiyGroup associations.
    Signed-off-by: Dan Smith <sonomofo@gmail.com>

--- Rhino.Security.Tests/DeleteEntityEventListenerFixture.cs
+++ Rhino.Security.Tests/DeleteEntityEventListenerFixture.cs
@@ -24,16 +24,36 @@
 
 			Assert.NotNull(reference);
 
-			// Delete account and confirm EntityReference was removed
+			// Create a permission for the entity
+			this.permissionsBuilderService.Allow("/Account/Edit").For(this.user).On(account).DefaultLevel().Save();
+
+			// Confirm the permissison for the entity exists
+			Permission permission = session.CreateCriteria<Permission>()
+				.Add(Restrictions.Eq("EntitySecurityKey", securityKey))
+				.SetCacheable(true)
+				.UniqueResult<Permission>();
+
+			Assert.NotNull(permission);
+
+			// Delete account 
 			session.Delete(account);
 			session.Flush();
 
+			// Confirm EntityReference was removed
 			reference = session.CreateCriteria<EntityReference>()
 				 .Add(Restrictions.Eq("EntitySecurityKey", securityKey))
 				 .SetCacheable(true)
 				 .UniqueResult<EntityReference>();
 
 			Assert.Null(reference);
+
+			// Confirm the permissison for the entity was removed
+			permission = session.CreateCriteria<Permission>()
+				.Add(Restrictions.Eq("EntitySecurityKey", securityKey))
+				.SetCacheable(true)
+				.UniqueResult<Permission>();
+
+			Assert.Null(permission);
 		}
 	}
 }
--- Rhino.Security/DeleteEntityEventListener.cs
+++ Rhino.Security/DeleteEntityEventListener.cs
@@ -1,13 +1,10 @@
 ﻿namespace Rhino.Security
 {
 	using System;
-	using NHibernate.Event;
-	using Microsoft.Practices.ServiceLocation;
-	using Rhino.Security.Interfaces;
 	using System.Collections.Generic;
-	using Rhino.Security.Model;
-	using Rhino.Security.Services;
 	using NHibernate.Criterion;
+	using NHibernate.Event;
+	using Rhino.Security.Model;
 
 	/// <summary>
 	/// Litenens for when a secured entity is deleted from the system and deletes 
@@ -27,14 +24,39 @@
 			var securityKey = Security.ExtractKey(deleteEvent.Entity);
 
 			if (!Guid.Empty.Equals(securityKey)) {
-				EntityReference reference = deleteEvent.Session.CreateCriteria<EntityReference>()
+				EntityReference entityReference = deleteEvent.Session.CreateCriteria<EntityReference>()
 					 .Add(Restrictions.Eq("EntitySecurityKey", securityKey))
 					 .SetCacheable(true)
 					 .UniqueResult<EntityReference>();
 
-				if (reference != null) {
+				if (entityReference != null) {
 					var childSession = deleteEvent.Session.GetSession(NHibernate.EntityMode.Poco);
-					childSession.Delete(reference);
+
+					childSession.Delete(entityReference);
+
+					//Also remove EntityReferencesToEntitiesGroups and Permissions that reference this entity
+
+					//Get list of EntitiesGroups that have the entity as a member
+					IEnumerable<EntitiesGroup> entitiesGroups = childSession.CreateCriteria<EntitiesGroup>()
+						.CreateCriteria("Entities")
+						.Add(Restrictions.Eq("EntitySecurityKey", securityKey))
+						.SetCacheable(true)
+						.List<EntitiesGroup>();
+
+					foreach (EntitiesGroup group in entitiesGroups) {
+						group.Entities.Remove(entityReference);
+					}
+
+					////Get list of Permissions that references the entity
+					IEnumerable<Permission> permissions = childSession.CreateCriteria<Permission>()
+						.Add(Restrictions.Eq("EntitySecurityKey", securityKey))
+						.SetCacheable(true)
+						.List<Permission>();
+
+					foreach (Permission permission in permissions) {
+						childSession.Delete(permission);
+					}
+
 					childSession.Flush();
 				}
 			}
