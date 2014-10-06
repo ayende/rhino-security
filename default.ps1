349f8c596113c932cd251dc2132c3b793de6293d
Mark Junker <mark.junker75@gmail.com>
2014-10-06 11:21:23 +0200

    Version auf 1.3.1 erhöht

--- default.ps1
+++ default.ps1
@@ -4,7 +4,7 @@ properties {
   $build_dir = "$base_dir\build" 
   $buildartifacts_dir = "$build_dir\" 
   $sln_file = "$base_dir\Rhino.Security-vs2008.sln" 
-  $version = "1.3.0.0"
+  $version = "1.3.1"
   $humanReadableversion = "1.3"
   $tools_dir = "$base_dir\Tools"
   $release_dir = "$base_dir\Release"
